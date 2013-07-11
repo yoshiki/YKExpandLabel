a5c257f5d2b3fd0616c1a54bffbdd8fc02706238
Yoshiki Kurihara <clouder@gmail.com>
2013-07-11 15:33:18 +0900

    Initial Commit

---
+++ YKExpandLabelDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YKExpandLabelDemo
+//
+//  Created by Yoshiki Kurihara on 2013/07/11.
+//  Copyright (c) 2013年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YKExpandLabelDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YKExpandLabelDemo
+//
+//  Created by Yoshiki Kurihara on 2013/07/11.
+//  Copyright (c) 2013年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
